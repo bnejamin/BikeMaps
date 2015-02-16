700cbdbf05e6e92b9603e70a66aa8281fafc7321
Taylor Denouden <tayden123@hotmail.com>
2015-02-16 15:55:36 -0800

    create hazard and theft migration scripts

---
+++ migrationScripts/migrateThefts.py
@@ -0,0 +1,50 @@
+import sys, os, django, json
+
+# Get Django settings and shell commands
+sys.path.append( os.path.dirname(os.getcwd()) )
+os.environ['DJANGO_SETTINGS_MODULE'] = 'VicBikeMap.settings.dev'
+django.setup()
+
+from django.contrib.gis.geos import GEOSGeometry
+
+from mapApp.models import Theft
+
+# USAGE
+# $ python migrateIncidents.py jsonFileToLoad_date.json
+def main(argv):
+    # Open file
+    json_data = open(argv[0]).read()
+    data = json.loads(json_data)
+
+    for f in data['features']:
+        # print json.dumps(f, indent=2, separators=(',', ': '))
+        # Set properties
+        p = f['properties']
+        theft = Theft(
+            geom = GEOSGeometry(json.dumps( f['geometry'] )),
+
+            report_date = p["date"],
+
+            date = p["theft_date"],
+            theft_type = p["theft"],
+            how_locked = p["how_locked"],
+            lock = p["lock"],
+            locked_to = p["locked_to"],
+            lighting = p["lighting"],
+            traffic = p["traffic"],
+
+            details = p["theft_detail"],
+
+            regular_cyclist = p["regular_cyclist"],
+            police_report = p["police_report"],
+            police_report_num = p["police_report_num"],
+            insurance_claim = p["insurance_claim"],
+            insurance_claim_num = p["insurance_claim_num"],
+
+            p_type = "theft",
+        )
+
+        theft.save()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
